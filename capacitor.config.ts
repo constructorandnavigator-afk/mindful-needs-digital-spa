1748e0d5c8e0100c6ec2cc160bac228af5bc8e06
constructorandnavigator-afk <constructorandnavigator@yahoo.com>
2025-09-29 11:24:33 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mindful Needs Digital Spa',
+  webDir: 'dist'
+};
+
+export default config;
